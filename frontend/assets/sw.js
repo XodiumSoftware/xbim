a2b0fe392451d15ea5b5ba5f7e9d34506b703f9f
illyrius666 <fitimq@live.nl>
2025-01-07 12:08:39 +0100

    optimized sw.js
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- frontend/assets/sw.js
+++ frontend/assets/sw.js
@@ -1,20 +1,19 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ + Copyright (c) 2025. Xodium.
+ + All rights reserved.
+ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
 const cacheName = "xodium_xbim_pwa";
 const filesToCache = ["./", "./index.html", "./xbim.js", "./xbim_bg.wasm"];
 
 /* Start the service worker and cache all the app's content */
-self.addEventListener("install", function (e) {
+self.addEventListener("install", e =>
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
-});
+        caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    ));
 
 /* Serve cached content when offline */
-self.addEventListener("fetch", function (e) {
+self.addEventListener("fetch", e =>
     e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
-});
+        caches.match(e.request).then(response => response || fetch(e.request))
+    ));
