412ae23a227eab202bd9d5d2fca6d3a1e15425c0
illyrius666 <fitimq@live.nl>
2025-04-07 15:50:30 +0200

    using build in figment config system instead, removing unused key, changing api_key to Uuid and letting it generate one on config create to avoid empty key exploit when setting up.
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/config.rs
+++ src/config.rs
@@ -3,52 +3,30 @@
  * All rights reserved.
  */
 
+use rocket::serde::uuid::Uuid;
 use rocket::serde::{Deserialize, Serialize};
-use std::{env, error, fs, io};
 
 /// Configuration settings for the application.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
-pub struct Config {
-    pub server_port: u16,
+pub struct AppConfig {
     pub database_url: String,
     pub database_username: String,
     pub database_password: String,
-    pub api_key: String,
+    pub api_key: Uuid,
     pub tls_cert_path: Option<String>,
     pub tls_key_path: Option<String>,
 }
 
-impl Default for Config {
+impl Default for AppConfig {
     fn default() -> Self {
         Self {
-            server_port: 8001,
             database_url: "db.xodium.org".to_string(),
             database_username: String::new(),
             database_password: String::new(),
-            api_key: String::new(),
+            api_key: Uuid::now_v7(),
             tls_cert_path: None,
             tls_key_path: None,
-        }
-    }
-}
-
-impl Config {
-    pub fn init() -> Result<Self, Box<dyn error::Error>> {
-        let config_path = env::current_exe()?
-            .parent()
-            .ok_or("Failed to get executable directory")?
-            .join("config.toml");
-
-        match fs::read_to_string(&config_path) {
-            Ok(content) => Ok(toml::from_str(&content)?),
-            Err(e) if e.kind() == io::ErrorKind::NotFound => {
-                let config = Self::default();
-                fs::write(&config_path, toml::to_string_pretty(&config)?)?;
-                println!("Created new config file at: {}", config_path.display());
-                Ok(config)
-            }
-            Err(e) => Err(e.into()),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,11 @@ pub mod config;
 pub mod database;
 pub mod errors;
 
+use crate::config::AppConfig;
 use database::Database;
 use errors::catchers;
+use figment::providers::{Format, Serialized, Toml};
+use figment::Figment;
 use rocket::{
     build, config::TlsConfig, launch, routes, shield::ExpectCt, shield::Feature, shield::Frame,
     shield::Hsts, shield::NoSniff, shield::Permission, shield::Prefetch, shield::Referrer,
@@ -30,21 +33,18 @@ use rocket::{
 use rocket_async_compression::{Compression, Level as CompressionLevel};
 use rocket_cors::{AllowedOrigins, CorsOptions};
 use routes::{health::health, ifc::delete_ifc, ifc::get_ifc, ifc::update_ifc, ifc::upload_ifc};
-use std::process::exit;
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
-    let config = match config::Config::init() {
-        Ok(cfg) => cfg,
-        Err(e) => {
-            eprintln!("Failed to load configuration: {}", e);
-            exit(1);
-        }
-    };
-    let mut rocket_config = Config {
-        port: config.server_port,
-        ..Config::debug_default()
-    };
+    let figment =
+        Figment::from(Serialized::defaults(AppConfig::default())).merge(Toml::file("config.toml"));
+
+    let config = figment.extract::<AppConfig>().unwrap_or_else(|err| {
+        println!("Configuration error (using defaults): {}", err);
+        AppConfig::default()
+    });
+
+    let mut rocket_config = Config::default();
 
     if let (Some(cert_path), Some(key_path)) = (&config.tls_cert_path, &config.tls_key_path) {
         if !cert_path.is_empty() && !key_path.is_empty() {
@@ -52,10 +52,12 @@ async fn rocket() -> Rocket<Build> {
         }
     }
 
+    let db = Database::new(&config).await;
+
     build()
         .configure(rocket_config)
-        .manage(config.clone())
-        .manage(Database::new(&config).await)
+        .manage(config)
+        .manage(db)
         .mount(
             "/",
             routes![health, upload_ifc, get_ifc, update_ifc, delete_ifc],
